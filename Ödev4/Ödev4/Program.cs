1e687522b2cd08003ce29d163514aaadaccec40e
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-02 00:31:49 +0300

    4.Ödevi yaptım

---
+++ Ödev4/Ödev4/Program.cs
@@ -0,0 +1,13 @@
+﻿//array (dizi), aynı türde birden fazla veriyi saklamak için kullanılan bir veri yapısıdır.
+//Diziler, bellekte ardışık olarak yerleştirilir ve dizinin her bir elemanına indeks (index) numarası ile erişilir.
+//Dizinin elemanlar sıfırdan başlar
+
+
+
+// Örnek 1
+
+int[] sayilar = { 10, 20, 30 };
+
+Console.WriteLine(sayilar[0]); 
+Console.WriteLine(sayilar[1]); 
+Console.WriteLine(sayilar[2]);
