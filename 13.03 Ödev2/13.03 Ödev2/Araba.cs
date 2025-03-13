cf7d718a47a224a9e4ce13ec8f17545a08e9cc7b
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-13 13:33:21 +0300

    13.03 Ödev2

---
+++ 13.03 Ödev2/13.03 Ödev2/Araba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._03_Ödev2
+{
+    public class Araba
+    {
+        public int yilHesapla(int yıl)
+        {
+            int arabaYili = 0;
+            arabaYili= DateTime.Now.Year - yıl;
+            return arabaYili;
+        }
+    }
+}
---
+++ 13.03 Ödev2/13.03 Ödev2/Program.cs
@@ -0,0 +1,21 @@
+﻿using _13._03_Ödev2;
+
+Araba a = new Araba();
+int gelenYas = a.yilHesapla(1980);
+
+if (gelenYas > 0 && gelenYas < 10)
+{
+    Console.WriteLine("arabanız yeni");
+}
+else if (gelenYas > 10 && gelenYas < 20)
+{
+    Console.WriteLine("servise götürmeniz gerekebilir");
+}
+else if (gelenYas > 20 && gelenYas < 30)
+{
+    Console.WriteLine("arabanız hurdaya çıkabilir");
+}
+else
+{
+    Console.WriteLine("Ya hiç üretilmedi ya da trafikten men edilmiştir");
+}
