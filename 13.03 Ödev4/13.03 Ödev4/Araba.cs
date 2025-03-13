4d02b28657e4017a0d3f52a3d96bf7a5f3c103c3
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-13 16:28:30 +0300

    13.03 Ödev4

---
+++ 13.03 Ödev4/13.03 Ödev4/Araba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._03_Ödev4
+{
+    public class Araba
+    {
+       public string model;
+       public int benzin; 
+    }
+}
---
+++ 13.03 Ödev4/13.03 Ödev4/Program.cs
@@ -0,0 +1,27 @@
+﻿using _13._03_Ödev4;
+
+Araba a = new Araba();  
+Araba b = new Araba();  
+Araba c = new Araba();
+
+
+a.model = "BMW";
+b.model = "Porsche";
+c.model = "Mercedes";
+
+a.benzin = 20;
+b.benzin = 30;
+c.benzin = 40;
+
+string[] bilgiler = new string[3];
+bilgiler[0] = a.model + " " + a.benzin;
+bilgiler[1] = b.model + " " + b.benzin;
+bilgiler[2] = c.model + " " + c.benzin;
+
+for(int i = 0; i < bilgiler.Length; i++)
+{
+    Console.WriteLine(bilgiler[i]);
+}
+int toplamBenzin = 0;
+toplamBenzin += a.benzin + b.benzin + c.benzin;
+Console.WriteLine("Toplam benzin = " + toplamBenzin);
