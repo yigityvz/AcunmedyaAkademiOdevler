cea43e7f73a52bc7ecd7859547f7447b7b691aaa
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-13 13:16:22 +0300

    13.03 Ödev1

---
+++ 13.03 Ödev1/13.03 Ödev1/Insan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._03_Ödev1
+{
+    public class Insan
+    {
+        public int yasHesapla(int dogumYili)
+        {
+            int sonuc = 0;
+            sonuc = DateTime.Now.Year - dogumYili;
+            return sonuc;
+
+        }
+    }
+}
---
+++ 13.03 Ödev1/13.03 Ödev1/Program.cs
@@ -0,0 +1,29 @@
+﻿using _13._03_Ödev1;
+
+Insan i = new Insan();
+int gelenYas = i.yasHesapla(1980);
+
+if (gelenYas > 0 && gelenYas < 18)
+{
+    Console.WriteLine("küçüksünüz");
+}
+else if (gelenYas > 18 && gelenYas < 35)
+{
+    Console.WriteLine("gençsiniz");
+}
+else if (gelenYas > 35 && gelenYas < 55)
+{
+    Console.WriteLine("yetişkinsiniz");
+}
+else if (gelenYas > 55 && gelenYas < 75)
+{
+    Console.WriteLine("yaşlısınız");
+}
+else if (gelenYas > 75 && gelenYas < 99)
+{
+    Console.WriteLine("çok yaşlısınız");
+}
+else
+{
+    Console.WriteLine("Ya hiç doğmadınız ya da çoktan öldünüz");
+}
