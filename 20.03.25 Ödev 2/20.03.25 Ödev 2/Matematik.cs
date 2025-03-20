7aafc330d4f28f4ab24f4faae9e8b733359c3909
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-20 16:43:23 +0300

    20.03.25 Ödev2

---
+++ 20.03.25 Ödev 2/20.03.25 Ödev 2/Matematik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._03._25_Ödev_2
+{
+    public class Matematik
+    {
+        public int Topla(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Topla(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public double Carp(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Carp(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+    }
+
+
+}
---
+++ 20.03.25 Ödev 2/20.03.25 Ödev 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using _20._03._25_Ödev_2;
+
+Matematik matematik = new Matematik();
+
+int toplama1 = matematik.Topla(10, 20);
+int toplama2 = matematik.Topla(10, 20, 30);
+
+
+Console.WriteLine("2 Sayı Toplamı: " + toplama1);
+Console.WriteLine("3 Sayı toplamı: " + toplama2);
+
+
+
+
+double carpma1 = matematik.Carp(1.5, 2.5);
+double carpma2 = matematik.Carp(1.5, 2.5, 3.5);
+
+Console.WriteLine("2 Sayı çarpımı: " + carpma1);
+Console.WriteLine("3 Sayı çarpımı: " + carpma2);
