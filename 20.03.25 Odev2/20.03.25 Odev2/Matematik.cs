e957c496873416b1d3fdcd5512216a0cf4aa2ade
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-20 16:34:37 +0300

    20.03.25 Odev2

---
+++ 20.03.25 Odev2/20.03.25 Odev2/Matematik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._03._25_Odev2
+{
+    public class Matematik
+    {
+        public int Topla(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Topla(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public double Carp(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Carp(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+    }
+}
