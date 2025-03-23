ffcba226316d359cb4e63e71f9793b475b0fa0a8
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-23 09:18:23 +0300

    23.03.25 Ödev6

---
+++ 23.03.25 Ödev6/23.03.25 Ödev6/Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+int targetNumber = random.Next(1, 101); // 1 ile 100 arasında rastgele sayı oluştur
+int guess = 0;
+int attempts = 0;
+
+Console.WriteLine("Sayı Tahmin Oyununa Hoş Geldiniz! 1 ile 100 arasında bir sayı tahmin edin.");
+
+while (guess != targetNumber)
+{
+    Console.Write("Tahmininiz: ");
+
+    while (!int.TryParse(Console.ReadLine(), out guess))
+    {
+        Console.Write("Geçerli bir sayı giriniz: ");
+    }
+
+    attempts++;
+
+    if (guess < targetNumber)
+    {
+        Console.WriteLine("Daha büyük bir sayı girin!");
+    }
+    else if (guess > targetNumber)
+    {
+        Console.WriteLine("Daha küçük bir sayı girin!");
+    }
+}
+
+Console.WriteLine($"Tebrikler! {attempts} denemede doğru tahmini yaptınız.");
