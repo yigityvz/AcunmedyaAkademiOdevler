c02b03e4044c731875c41d7d41b225b753521aee
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-23 09:15:59 +0300

    23.03.25 Ödev5

---
+++ 23.03.25 Ödev5/23.03.25 Ödev5/Hesaplama.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._03._25_Ödev5
+{
+    public class Hesaplama
+    { 
+            public int Topla(int sayi1, int sayi2)
+            {
+                return sayi1 + sayi2;
+            }
+
+            public int Carp(int sayi1, int sayi2)
+            {
+                return sayi1 * sayi2;
+            }
+        
+    }
+}
---
+++ 23.03.25 Ödev5/23.03.25 Ödev5/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using _23._03._25_Ödev5;
+
+Hesaplama hesaplama = new Hesaplama();
+
+int toplamSonucu = hesaplama.Topla(10, 5);
+int carpimSonucu = hesaplama.Carp(10, 5);
+
+Console.WriteLine("Toplam:" + toplamSonucu);
+Console.WriteLine("Çarpım:" + carpimSonucu);
