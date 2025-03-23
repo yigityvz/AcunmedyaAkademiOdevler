c9930503e741755f273420613a49b692c00da398
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-23 08:46:37 +0300

    23.03.25 Ödev2

---
+++ 23.03.25 Ödev2/23.03.25 Ödev2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+int[] sayilar = { 10, 20, 30, 40, 50 };
+int toplam = 0;
+
+
+foreach (int sayi in sayilar)
+{
+    toplam += sayi;
+}
+
+double ortalama = toplam / 5.0;
+
+
+Console.WriteLine("Girilen sayıların toplamı:" +  toplam);
+Console.WriteLine("Sayıların ortalaması:" + ortalama);
+    
