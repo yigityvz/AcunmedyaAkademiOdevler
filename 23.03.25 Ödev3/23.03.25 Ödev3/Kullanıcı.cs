401ec6e71deb254407ba9f6f0421b4878c949b30
yigityvz <yigit234272@ogr.duzce.edu.tr>
2025-03-23 09:06:23 +0300

    23.03.25 Ödev3

---
+++ 23.03.25 Ödev3/23.03.25 Ödev3/Kullanıcı.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._03._25_Ödev3
+{
+    public class Kullanıcı
+    {
+        private string adSoyad;
+        private int yas;
+        public string email { get; set; }
+
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string ad)
+        {
+            adSoyad = ad;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int age)
+        {
+            if (age > 0)
+                yas = age;
+            else
+                Console.WriteLine("Yaş negatif olamaz!");
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Ad Soyad:" + adSoyad + "Yaş:" + yas + "Email:" + email);
+        }
+    }
+}
---
+++ 23.03.25 Ödev3/23.03.25 Ödev3/Program.cs
@@ -0,0 +1,9 @@
+﻿using _23._03._25_Ödev3;
+
+    Kullanıcı kullanici = new Kullanıcı();
+    kullanici.SetAdSoyad("Yiğit Yavuz Tok");
+    kullanici.SetYas(20);
+    kullanici.email = "yigityavuztok@gmail.com";
+
+    kullanici.BilgileriGoster();
+
